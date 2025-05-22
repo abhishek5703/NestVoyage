4f952069ceb11be3507ae2b433a0c88cc9e55553
Abhishek Kumar <abhishek2018kumar12@gmail.com>
2025-05-22 11:02:28 +0530

    update user.js

--- controllers/users.js
+++ controllers/users.js
@@ -14,7 +14,7 @@ module.exports.signup = async (req, res) => {
             if (err) {
                 return next();
             }
-            req.flash("success", "Welcome to Wanderlust");
+            req.flash("success", "Welcome to NestVoyage!");
             res.redirect("/listings");
         })
     
@@ -29,7 +29,7 @@ module.exports.renderLoginForm = (req, res) => {
 };
 
 module.exports.login = async (req, res) => {
-    req.flash("success", "Welcome back to Wanderlust!");
+    req.flash("success", "Welcome back to NestVoyage!");
     let redirectUrl = res.locals.redirectUrl || "/listings";
     res.redirect(redirectUrl);
 };
